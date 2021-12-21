cc29dc569995e5bed8527d59e490a7a9c1cb543d
慢吞吞 <wenweixin.maxman@foxmail.com>
2021-12-21 16:11:23 +0800

    增加广播例子

--- src/ws/ws.adapter.ts
+++ src/ws/ws.adapter.ts
@@ -1,14 +1,31 @@
 import * as WebSocket from 'ws';
+
+import {
+    // ERROR_EVENT,
+    CLOSE_EVENT,
+    CONNECTION_EVENT,
+    DISCONNECT_EVENT,
+  } from '@nestjs/websockets/constants';
 import { WebSocketAdapter, INestApplicationContext } from '@nestjs/common';
 import { MessageMappingProperties } from '@nestjs/websockets';
 import { Observable, fromEvent, EMPTY } from 'rxjs';
 import { mergeMap, filter } from 'rxjs/operators';
 import { TSBuffer, DecodeOutput} from 'tsbuffer';
-import { TransportDataUtil, ServiceMap, ServiceMapUtil, ApiService, } from 'tsrpc-base-client'
+import { 
+    TransportDataUtil, 
+    ServiceMap, 
+    ServiceMapUtil, 
+    ApiService, 
+    Counter,
+} from 'tsrpc-base-client'
 import { ServerInputData, ServerOutputData } from 'tsrpc-proto';
-import { serviceProto } from '../shared/protocols/serviceProto';
+import { serviceProto, ServiceType } from '../shared/protocols/serviceProto';
+import { type } from 'os';
 // import {WsServer as TsrpcWsServer} from 'tsrpc';
 
+const TSRPC_INUSE = false;  // tsrpc编码开关
+
+type myConnection = WebSocket & {id: number};
 
 /**
  * todo 使用WebSocketAdapter构建tsrpcadapter?
@@ -16,15 +33,18 @@ import { serviceProto } from '../shared/protocols/serviceProto';
  * 怎么使用tsrpc的映射
  */
 export class WsAdapter implements WebSocketAdapter {
-    private conn_num = 0;
-    private serviceMap: ServiceMap;
-    private tsbuffer: TSBuffer;
+    conns: Array<myConnection> = [];
+    serviceMap: ServiceMap;
+    tsbuffer: TSBuffer;
+    _counter: Counter = new Counter(1);
+    
 
     constructor(private app: INestApplicationContext) { }
 
     create(port: number, options: any = {}): any {
         // 注册tsrpc servicemap,方便获取名字
         this.serviceMap = ServiceMapUtil.getServiceMap(serviceProto);  // 映射服务方法
+        console.log(this.serviceMap);
         this.tsbuffer = new TSBuffer({
             ...serviceProto.types,
             // 其他类型
@@ -36,27 +56,70 @@ export class WsAdapter implements WebSocketAdapter {
         console.log('ws create')
         return new WebSocket.Server({ port, ...options });
     }
-
+    /**
+     * 绑定客户端连接处理方法,当服务器启动时调用
+     * @param server WebSocket
+     * @param callback gateway 的 handleConnection 方法,如果有重载的话
+     */
     bindClientConnect(server, callback: Function) {
         // server: WebSocketServer?
         console.log('ws bindClientConnect, server:\n');
-        server.on('connection', callback);
+        server.on(CONNECTION_EVENT, (client: myConnection, incomeMsg) => {
+            client.id = this._counter.getNext();
+            this.conns.push(client);
+            console.log('[waAdapter]有新的连接进来', this.conns.length);
+            const msg = {
+                // 公共属性
+                time: new Date(),
+                // memberjoin
+                name: 'nhaha'+this.conns.length,
+                member: {
+                    name: 'kelvin',
+                    age: 33,
+                },
+                // chat
+                content: 'chaah'+this.conns.length,
+                
+            }
+            this._broadCast('MemberJoin', msg);
+            // todo 可以做一些逻辑
+            callback(client);
+        });
     }
-    // 监听连接断开??,每次连接都会触发
-    bindClientDisconnect(client: WebSocket, callback: Function) {
-        console.log('[wsAdapter]连接断开');
+    /**
+     * 绑定客户端断开处理方法,当客户端连接是调用
+     * @param client WebSocket
+     * @param callback gateway 的 handleDisconnect 方法,如果有重载的话
+     */
+    bindClientDisconnect(client: WebSocket, callback: any) {
+        const self = this;
+        client.on(CLOSE_EVENT, function(code: number, reason: Buffer){
+            const client = this as myConnection;
+            self.conns = self.conns.filter(({id}) => client.id !== id);
+            console.log('[waAdapter]有连接断开', self.conns.length);
+            // todo 客户端断开连接做逻辑
+            callback(this, code, reason);
+        });
     }
+    /**
+     * 只是消息绑定方法,仅用于绑定消息下游
+     * @param client WebSocket
+     * @param handlers 
+     * @param process 
+     */
     bindMessageHandlers(
         client: WebSocket,
         handlers: MessageMappingProperties[],
         process: (data: any) => Observable<any>,
     ) {
-        this.conn_num++;
-        console.log('[waAdapter]有新的连接进来', this.conn_num);
         fromEvent(client, 'message')
             .pipe(
                 mergeMap(data => {
-                    return this._bindTsbufferMessageHandler(client, data, handlers, process)
+                    if (TSRPC_INUSE) {
+                        return this._bindTsbufferMessageHandler(client, data, handlers, process);
+                    }else {
+                        return this._bindMessageHandler(client, data, handlers, process);
+                    }
                 }),
                 filter(result => {
                     console.log({r: result});   // 上一步处理好的数据(完整编码的tsbuffer)
@@ -64,23 +127,86 @@ export class WsAdapter implements WebSocketAdapter {
                 }),
             )
             .subscribe(response => {
-                // console.log(response);
-                client.send(response);  // 发送二进制内容
-                // client.send(JSON.stringify(response));
+                response = TSRPC_INUSE? response: JSON.stringify(response); // 发送二进制内容
+                client.send(response);
             });
     }
+    // 明文
+    _bindMessageHandler(
+        client: WebSocket,
+        buffer,
+        handlers: MessageMappingProperties[],
+        process: (data: any) => Observable<any>,
+    ): Observable<any> {
+        let message = null;
+        try {
+            message = JSON.parse(buffer.data);
+        } catch (error) {
+            console.log('ws解析json出错', error);
+            return EMPTY;
+        }
 
+        const messageHandler = handlers.find(
+            handler => handler.message === message.event,
+        );
+        if (!messageHandler) {
+            return EMPTY;
+        }
+        return process(messageHandler.callback(message.data));
+    }
+    // tsrpc解码
     _bindTsbufferMessageHandler(    // 解码,执行对应方法,编码
         client: WebSocket,
         buffer,
         handlers: MessageMappingProperties[],
         process: (data: any) => Observable<any>,
     ): Observable<any> {
+        const req = this._bufDecode(buffer.data);
+        if (!req) return EMPTY; // decode失败
+        const messageHandler = handlers.find(handler => {   // handler 就是我们@SubscribeMessage装饰过的方法
+            console.error(handler.message); // 处理方法名
+            return handler.message === req.service.name;
+        });
+        if (!messageHandler) {
+            return EMPTY;
+        }
+        // 构造Promise
+        let next = Promise.resolve(messageHandler.callback(req.val)).then(ret => {
+            console.log({sn: req.input.sn});
+            console.log({ret});
+            const _out = this._bufEncode(ret, req.service, req.input);
+            if (!_out) return null;
+            return _out.buf;
+        })
+        // 返回异步处理
+        return process(next);
+    }
+
+    close(server) {
+        // server.on(CLOSE_EVENT)
+        console.log('ws server close');
+        server.close();
+    }
+
+    _bufEncode(val: any, service: ApiService, input: ServerInputData): {buf: Uint8Array} | false {
+        let op = this.tsbuffer.encode(val, service.resSchemaId);    // 根据返回结构ID编码二进制内容
+        let serverOutputData: ServerOutputData = {
+            sn: input.sn,
+            serviceId: input.serviceId,
+            buffer: op.buf,
+        };
+        op = TransportDataUtil.tsbuffer.encode(serverOutputData, 'ServerOutputData');   // 外层协议编码
+        return {
+            buf: op.buf
+        };
+    }
+
+    _bufDecode(buf): {service: ApiService, val: any, input: ServerInputData} | false {
         // tsbuffer解码
-        let opServerInputData = TransportDataUtil.tsbuffer.decode(buffer.data, 'ServerInputData'); // 外层协议解码
+        let opServerInputData = TransportDataUtil.tsbuffer.decode(buf, 'ServerInputData'); // 外层协议解码
         if (!opServerInputData.isSucc) {
             console.error('解析TSPRC数据包外层失败');
-            return EMPTY;
+            return false;
         }
         let serverInput = opServerInputData.value as ServerInputData;   // 内层二进制内容
         let service = this.serviceMap.id2Service[serverInput.serviceId] as ApiService;
@@ -88,36 +214,23 @@ export class WsAdapter implements WebSocketAdapter {
         if (service.type === 'api') {
             onReq = this.tsbuffer.decode(serverInput.buffer, service.reqSchemaId);  // 根据请求结构ID解码明文内容
         }else {
-            // onReq = this.tsbuffer.decode(serverInput.buffer, service.msgSchemaId);   // 消息请求
+            // onReq = this.tsbuffer.decode(serverInput.buffer, service.msgSchemaId);   // 消息请求(一般只有服务器主动推送,客户端消息还是使用请求吧)
         }
         console.log(onReq.value);   // 解码后的请求内容
         console.log({servicename: service.name});   // 请求方法名
-        const messageHandler = handlers.find(handler => {   // handler 就是我们@SubscribeMessage装饰过的方法
-            console.error(handler.message); // 处理方法名
-            return handler.message === service.name;
-        });
-        if (!messageHandler) {
-            return EMPTY;
-        }
-        // 构造Promise
-        let next = Promise.resolve(messageHandler.callback(onReq.value)).then(ret => {
-            console.log({sn: serverInput.sn});
-            console.log({ret});
-            let op = this.tsbuffer.encode(ret, service.resSchemaId);    // 根据返回结构ID编码二进制内容
-            let serverOutputData: ServerOutputData = {
-                sn: serverInput.sn,
-                serviceId: serverInput.serviceId,
-                buffer: op.buf,
-            };
-            op = TransportDataUtil.tsbuffer.encode(serverOutputData, 'ServerOutputData');   // 外层协议编码
-            return op.buf;
-        })
-        // 返回异步处理
-        return process(next);
+        return {
+            input: serverInput,
+            service,
+            val: onReq.value,
+        };
     }
 
-    close(server) {
-        console.log('ws server close');
-        server.close();
+    _broadCast<T extends keyof ServiceType['msg']>(msgName: T, msg: ServiceType['msg'][T], conns?: myConnection[]) {
+        let service = this.serviceMap.msgName2Service[msgName as string];
+        if (!service) return false;
+        let opEncode = TransportDataUtil.encodeServerMsg(this.tsbuffer, service, msg, 'buffer', 'LONG');
+        if (!opEncode.isSucc) return false;
+        conns = conns? conns : this.conns;
+        conns.forEach(conn => conn.send(opEncode.output));
     }
 }
--- src/ws/ws.gateway.ts
+++ src/ws/ws.gateway.ts
@@ -1,17 +1,25 @@
-import { ConnectedSocket, MessageBody, SubscribeMessage, WebSocketGateway, WsResponse } from "@nestjs/websockets";
+import { 
+  ConnectedSocket, 
+  MessageBody, 
+  OnGatewayConnection, 
+  OnGatewayDisconnect, 
+  OnGatewayInit, 
+  SubscribeMessage, 
+  WebSocketGateway, 
+  WebSocketServer, 
+  WsResponse
+} from "@nestjs/websockets";
 import { Observable } from "rxjs";
 import * as WebSocket from 'ws';
 import {ReqSend, ResSend} from './../shared/protocols/PtlSend'
 
-interface Person {
-  name: string,
-  age: number,
-}
-
-// @WebSocketGateway(3002, {transports: ['websocket']}) // √
-@WebSocketGateway(3002)  // √
+@WebSocketGateway(3002, {transports: ['websocket']}) // √
+// @WebSocketGateway(3002)  // √
 // @WebSocketGateway()
-export class WsStartGateway {
+export class WsStartGateway implements OnGatewayConnection, OnGatewayDisconnect, OnGatewayInit{
+
+  @WebSocketServer() private server;
+  private _wsClients:Array<WebSocket> = [];
 
   private _sleep(ms: number = 1000): Promise<number> {
     return new Promise(function(resolve, reject) {
@@ -21,8 +29,21 @@ export class WsStartGateway {
     })
   }
 
+  afterInit(server: any) {
+    console.log('after init');
+  }
+
+  handleDisconnect(client: any) {
+    console.log('[gateway] client disconnect');;
+  }
+
+  handleConnection(client: WebSocket, ...args: any[]) { // client的类型??
+    console.log('[gateway] client connect');;
+  }
+
   @SubscribeMessage('Send')
   async sendmsg(@MessageBody() send: ReqSend): Promise<ResSend> {
+    // todo 在这里广播,消息如何进行tsrpc编码?
     console.log('invoke len');
     await this._sleep(1000);
     return {
@@ -30,12 +51,10 @@ export class WsStartGateway {
     };
   }
 
-  @SubscribeMessage('hello')
-  hello(@MessageBody() data: any): any {
+  @SubscribeMessage('Hello')
+  hello(@MessageBody() data: ReqSend): ResSend {
     return {
-      "event": "hello",
-      "data": data,
-      "msg": 'rustfisher.com'
+      time: new Date(),
     };
   }
 
